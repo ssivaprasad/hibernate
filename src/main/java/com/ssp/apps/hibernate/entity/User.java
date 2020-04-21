7612588ca7f249790559b1f4ad8ef1d5f8c8ac5b
SivaprasadS <somarouthu.sivaprasads@gmail.com>
2020-04-21 20:36:26 +0530

    Save Collections

--- src/main/java/com/ssp/apps/hibernate/entity/User.java
+++ src/main/java/com/ssp/apps/hibernate/entity/User.java
@@ -1,16 +1,19 @@
 package com.ssp.apps.hibernate.entity;
 
+import java.util.List;
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,23 +24,31 @@ import lombok.ToString;
 @Setter
 @Getter
 @NoArgsConstructor
-@ToString
+@ToString(exclude = {"socialProfiles"})
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_ID_GENERATOR")
-	@SequenceGenerator(name = "USER_ID_GENERATOR", sequenceName = "USER_SEQ")
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_ID_GENERATOR")
+    @SequenceGenerator(name = "USER_ID_GENERATOR", sequenceName = "USER_SEQ")
+    private Integer id;
 
-	private String name;
+    private String name;
 
-	@AttributeOverrides({@AttributeOverride(name = "number", column = @Column(name = "office_landline")),
-			@AttributeOverride(name = "mail", column = @Column(name = "office_email"))})
-	@Embedded
-	private Contact officeContact;
+    @AttributeOverrides({
+            @AttributeOverride(name = "number", column = @Column(name = "office_landline")),
+            @AttributeOverride(name = "mail", column = @Column(name = "office_email"))})
+    @Embedded
+    private Contact officeContact;
 
-	@AttributeOverrides({@AttributeOverride(name = "number", column = @Column(name = "mobile_number")),
-			@AttributeOverride(name = "mail", column = @Column(name = "email"))})
-	@Embedded
-	private Contact personalContact;
+    @AttributeOverrides({
+            @AttributeOverride(name = "number", column = @Column(name = "mobile_number")),
+            @AttributeOverride(name = "mail", column = @Column(name = "email"))})
+    @Embedded
+    private Contact personalContact;
+
+    @ElementCollection
+    @CollectionTable(name = "social_profiles", joinColumns = @JoinColumn(name = "social_id"))
+    private List<String> socialProfiles;
+
+
 }
--- src/test/java/com/ssp/apps/hibernate/dao/UserRepositoryTest.java
+++ src/test/java/com/ssp/apps/hibernate/dao/UserRepositoryTest.java
@@ -1,46 +1,50 @@
 package com.ssp.apps.hibernate.dao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.ssp.apps.hibernate.HibernateApplication;
 import com.ssp.apps.hibernate.entity.Contact;
 import com.ssp.apps.hibernate.entity.User;
-
 import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = HibernateApplication.class)
 @Slf4j
 public class UserRepositoryTest {
-	@Autowired
-	private UserRepository repository;
+    @Autowired
+    private UserRepository repository;
 
-	@Test
-	public void findById() {
-		Optional<User> userOptional = repository.findById(1);
-		if (userOptional.isPresent()) {
-			log.info("== >> {}", userOptional.get());
-		}
-	}
+    @Test
+    public void findById() {
+        Optional<User> userOptional = repository.findById(1);
+        if (userOptional.isPresent()) {
+            log.info("== >> {}", userOptional.get());
+        }
+    }
 
-	@Test
-	public void save() {
-		User user = new User();
-		user.setName("Siva");
+    @Test
+    public void save() {
+        User user = new User();
+        user.setName("Siva");
 
-		user.setOfficeContact(new Contact("72342723", "Siva@abc.com"));
-		user.setPersonalContact(new Contact("3475784", "siva1@gmail.com"));
+        user.setOfficeContact(new Contact("72342723", "Siva@abc.com"));
+        user.setPersonalContact(new Contact("3475784", "siva1@gmail.com"));
 
-		repository.save(user);
+        List<String> socialProfiles = new ArrayList<String>();
+        socialProfiles.add("Siva@twitter.com");
+        socialProfiles.add("Siva@linkedin.com");
+        user.setSocialProfiles(socialProfiles);
 
-		User user1 = repository.findById(user.getId()).get();
-		log.info("=== >>> {}", user1);
-	}
+        repository.save(user);
+
+        User user1 = repository.findById(user.getId()).get();
+        log.info("=== >>> {}", user1);
+    }
 
 }
